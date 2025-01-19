276ed728f8b759a3f25dfa3c73fcee679264c0d4
Leticia Ocada dos Santos <leticiaocada@MacBook-Air-de-Leticia.local>
2025-01-19 17:15:13 -0300

    Implementing flask read, update and delete

--- app.py
+++ app.py
@@ -15,5 +15,47 @@ def create_task():
     tasks.append(new_task)
     return jsonify({"message": "New task created"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+             }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Task not found"}), 404
+
+@app.route('tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in task:
+        if t.id == id:
+            task = t
+    if task == None:
+        return jsonify({"message": "task not found"}), 404 
+    data = request.get_json()
+    task.title = data.get("title")
+    task.description = data.get("description")
+    task.completed = data.get("completed")
+    return jsonify({"message": "Task updated"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if not task:
+        return jsonify({"message": "task not found"}), 404
+    tasks.remove(task)
+    return jsonify({"message": "Task deleted"})     
+
 if __name__ == "__main__":
     app.run(debug=True)
